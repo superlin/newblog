16d8e0b9eb4d8817ee16490aa8cbd06fc92be9a2
superlin <1092793900@qq.com>
2015-05-04 23:34:08 +0800

    修复没有h3时不显示目录

--- content/themes/Futura/assets/js/post.js
+++ content/themes/Futura/assets/js/post.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
     // 添加文章目录
-    var sidebar = $(".sidebar");
-    var allh3 = $(".main-content h3");
-    var html = '';
-    Array.prototype.slice.call(allh3, 0).forEach(function(h3,i){
-        var id = "h3-"+(i+1);
-        var txt = $(h3).html().trim();
-        h3.id = id;
-        html += "<li><a href='#"+id+"''>"+txt+"</a></li>";
-    });
-    html = '<div class="widget catalog-brief">'+
-              '<h4 class="title">Content</h4>' +
-              "<ul>"+html+"</ul>"+
-            '</div>';
-    sidebar.append(html);
+    function appendLists(allh3) {
+      var sidebar = $(".sidebar");
+      var html = '';
+      Array.prototype.slice.call(allh3, 0).forEach(function(h3,i){
+          var id = "h3-"+(i+1);
+          var txt = $(h3).html().trim();
+          h3.id = id;
+          html += "<li><a href='#"+id+"''>"+txt+"</a></li>";
+      });
+      html = '<div class="widget catalog-brief">'+
+                '<h4 class="title">Content</h4>' +
+                "<ul>"+html+"</ul>"+
+              '</div>';
+      sidebar.append(html);
+    }
 
     // scrollTop大小
-    var scTop = function() {
+    function scTop() {
         if( typeof window.pageYOffset != 'undefined') {
             return window.pageYOffset;
         } else if( typeof document.compatMode != 'undefined' && document.compatMode != 'BackCompat') {
@@ -27,7 +28,7 @@ $(document).ready(function(){
     }
 
     // 添加fixed
-    var addFixed = function(sideTop) {
+    function addFixed($catalog, sideTop) {
         if(sideTop < scTop()){
             if(!$catalog.hasClass('catalog-fixed')) $catalog.addClass("catalog-fixed");
         } else {
@@ -36,23 +37,35 @@ $(document).ready(function(){
     }
 
     // 监听scorll事件（使用函数节流，10帧）
-    var $catalog = $(".catalog-brief");
-    var sideTop = $catalog.offset().top;
-    var scrollTimer = null;
-    addFixed(sideTop);
-    $(window).scroll(function(){
-        if(scrollTimer) {
-            clearTimerout(scrollTimer);
-        }
-        resizeTimer = setTimeout(function(){
-            addFixed(sideTop);
-        }, 100);
-    });
+    function catlogFixed() {
+      var $catalog = $(".catalog-brief");
+      var sideTop = $catalog.offset().top;
+      var scrollTimer = null;
+      addFixed($catalog, sideTop);
+      $(window).scroll(function(){
+          if(scrollTimer) {
+              clearTimeout(scrollTimer);
+          }
+          scrollTimer = setTimeout(function(){
+              addFixed($catalog, sideTop);
+          }, 100);
+      });
+    }
 
     //添加音乐播放器
-    $(".music-panel iframe").attr("src", "http://music.163.com/outchain/player?type=0&id=67251026&auto=0&height=430");
-    $(".music-wrap a").click(function() {
-      $(this).toggleClass('on');
-      $(".music-panel").toggle();
-    });
+    function addMusic() {
+      $(".music-panel iframe").attr("src", "http://music.163.com/outchain/player?type=0&id=67251026&auto=0&height=430");
+      $(".music-wrap a").click(function() {
+        $(this).toggleClass('on');
+        $(".music-panel").toggle();
+      });
+    }
+
+    // 初始化
+    var allh3 = $(".main-content h3");
+    if (allh3.length > 0) {
+      appendLists(allh3);
+      catlogFixed();
+    }
+    addMusic();
 });
